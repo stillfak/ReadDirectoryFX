59de0b3b7d83450732e1c06f8e42c8d2f0213b69
vadim <vadim.gavrikov.1998@gmail.com>
2018-03-14 18:45:19 +0300

    добавленны действия для кнопки open

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,28 +1,46 @@
 package sample;
 
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
+    @FXML
+    private Label path;
 
     @FXML
-    private ResourceBundle resources;
+    private TextField input;
 
     @FXML
-    private URL location;
+    private Button open;
 
     @FXML
-    private Button button;
-
+    private Button search;
     @FXML
     void initialize() {
-        assert button != null : "fx:id=\"button\" was not injected: check your FXML file 'sample.fxml'.";
-//        button.setOnAction(event -> System.out.println("Ты пидор"));
+        assert open != null : "fx:id=\"open\" was not injected: check your FXML file 'sample.fxml'.";
+        assert search != null : "fx:id=\"search\" was not injected: check your FXML file 'sample.fxml'.";
+        File[] file1 = new File[1];
+        open.setOnAction(event -> {
+            DirectoryChooser directory = new DirectoryChooser();
+//            fileChooser.setInitialDirectory();
+            directory.setTitle("Select directory for open");
+//            directory.setInitialDirectory(new File(String.valueOf(input.getCharacters())));
+
+
+            file1[0] = new File(String.valueOf(directory.showDialog(new Stage()).getPath()));
+//            System.out.println(file1[0].getPath());
+            path.setText(file1[0].getPath());
+        });
     }
 }
+
 
 
 
