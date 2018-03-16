bd736912c86d97d67aa155956ce267d813460cfa
vadim <vadim.gavrikov.1998@gmail.com>
2018-03-16 21:26:26 +0300

    метод searchDirectory перенесен в другой класс

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,7 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Controller {
     @FXML
@@ -18,43 +19,43 @@ public class Controller {
     private TextField input;
 
     @FXML
+    private TextField type;
+
+    @FXML
     private Button open;
 
     @FXML
     private Button search;
+
+    @FXML
+    private Label statusReady;
+
+
+
+
     @FXML
     void initialize() {
-        assert open != null : "fx:id=\"open\" was not injected: check your FXML file 'sample.fxml'.";
-        assert search != null : "fx:id=\"search\" was not injected: check your FXML file 'sample.fxml'.";
+
         File[] file1 = new File[1];
         open.setOnAction(event -> {
             DirectoryChooser directory = new DirectoryChooser();
-//            fileChooser.setInitialDirectory();
             directory.setTitle("Select directory for open");
-//            directory.setInitialDirectory(new File(String.valueOf(input.getCharacters())));
-
-
-            file1[0] = new File(String.valueOf(directory.showDialog(new Stage()).getPath()));
-//            System.out.println(file1[0].getPath());
+            file1[0] = new File(String.valueOf(directory.showDialog(new Stage())));
             path.setText(file1[0].getPath());
+
         });
         search.setOnAction(event -> {
-            searchDirectory(file1[0],String.valueOf(input.getCharacters()));
+            try {
+                searchInFiles.starter(file1[0],String.valueOf(input.getCharacters()),String.valueOf(type.getCharacters()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            statusReady.setText("успешно");
         });
     }
-    private static void searchDirectory(File pathDirectory, String searchMsg) {
-        File[] files = pathDirectory.listFiles();
-        assert files != null;
 
-        for (File file : files) {
-
-            if (file.isFile() && file.getName().matches(".*\\.html$")) {
-                new searchInFiles(file, searchMsg).start();
-            } else if (file.isDirectory()) {
-                searchDirectory(file, searchMsg);
-            }
-        }
-    }
 }
 
 
