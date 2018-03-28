c8351815fab526579607e18070d5d2c5f8e77c32
vadim <vadim.gavrikov.1998@gmail.com>
2018-03-28 10:58:18 +0300

    добавленны комментарии для файла SearchInFiles.java

---
+++ src/sample/SearchInFiles.java
@@ -0,0 +1,134 @@
+package sample;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Класс осуществляет переход по ветки файловой системы,
+ * Ищет файл определенного типа(type введенного пользователем),
+ * если файл найден создается экземпляр этого класса,
+ * для поиска запроса пользователя в этом файле
+ *
+ * @author Gavrikov V 15it18
+ */
+
+public class SearchInFiles extends Thread {
+    private File fileForSearch;
+    private String strSearch;
+
+    private static ArrayList<SearchInFiles> searchInFiles;
+    private static volatile BufferedWriter fileResult;
+    private static int i = 0;
+
+    private SearchInFiles(File fileForSearch, String strSearch) {
+        this.fileForSearch = fileForSearch;
+        this.strSearch = strSearch;
+    }
+
+    @Override
+    public void run() {
+        String str;
+        Pattern searchStr = Pattern.compile(strSearch);
+
+
+        Matcher mSearchStr;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(fileForSearch))) {
+
+
+            for (int i = 1; (str = inFile.readLine()) != null; i++) {
+                mSearchStr = searchStr.matcher(str);
+                if (mSearchStr.find()) {
+                    write(fileForSearch.getPath() + ": " + mSearchStr.group() + "; line: " + i);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Промежуточный метод
+     * открытие и закрытие файла для записи в результирующий файл,
+     * вызов метода поиска файлов типа type,
+     * вызов метода, который жждет выполнение потоков
+     * и закрытие результирующего файла.
+     *
+     * @param pathDirectory стартовая директория
+     * @param searchMsg запрос пользователя
+     * @param type тип искомых фалов
+     * @throws IOException input output
+     * @throws InterruptedException
+     */
+    static void starter(File pathDirectory, String searchMsg, String type) throws IOException, InterruptedException {
+        fileResult = new BufferedWriter(new FileWriter(new File("result.txt")));
+        searchInFiles = new ArrayList<>();
+
+        searchFiles(pathDirectory, searchMsg, type);
+
+        aliveAndJoin(searchInFiles);
+
+        fileResult.close();
+    }
+
+    /**
+     * Метод записывает в файл строки
+     *
+     * @param str - строка для записи
+     * @throws IOException input output
+     */
+    private synchronized void write(String str) throws IOException {
+        fileResult.write(str + "\n");
+    }
+
+    /**
+     * Метод ищет файл определеного типа в определенной
+     * ветке файловой системы и вызывает создает
+     * экземпляр класса(наследник thread) и запускает его
+     *
+     * @param pathDirectory дирректория для поиска
+     * @param searchMsg запрос пользователя
+     * @param type тип файлов в которых нужно искать(расширение файла без точки)
+     */
+    private static void searchFiles(File pathDirectory, String searchMsg, String type) {
+        File[] files = pathDirectory.listFiles();
+        assert files != null;
+        try {
+            for (File file : files) {
+
+                if (file.isFile() && file.getName().matches(".*\\." + type + "$")) {
+                    searchInFiles.add(new SearchInFiles(file, searchMsg));
+
+                    searchInFiles.get(i).start();
+                    i++;
+                } else if (file.isDirectory()) {
+
+                    searchFiles(file, searchMsg, type);
+                }
+            }
+        }catch (NullPointerException | IndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * Метод проверяет выполняеться ли группа потоков из array list, если в результате проверки
+     * поток еще выполняется то вызвается метод join для этого потока.
+     *
+     * @param searchInFiles array list с эземплярами класса SearchInFiles
+     * @throws InterruptedException исключение заврешение потока
+     */
+    private static void aliveAndJoin(ArrayList<SearchInFiles> searchInFiles) throws InterruptedException {
+        for (SearchInFiles searchInFile : searchInFiles) {
+            if (searchInFile.isAlive()) {
+                searchInFile.join();
+            }
+        }
+    }
+
+}
