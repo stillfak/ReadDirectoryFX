1a32e89b401031a5c6cc573886d212bb6a4f35a5
vadim <vadim.gavrikov.1998@gmail.com>
2018-03-31 11:49:13 +0300

    пользователю заблокированно изменение размера окна

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Search");
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
